8c82e58f085b566356ca20ff721c56fe2e4f3412
nathanfrib <nnathanfausttino@gmail.com>
2025-02-09 15:48:20 -0300

    Adicionando servidor v3

--- src/main.py
+++ src/main.py
@@ -1,82 +1,96 @@
-import requests
-import time
-from datetime import datetime
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-from database import BitcoinPrice, Base
-import os
-from dotenv import load_dotenv
+import requests  
+import time  
+from datetime import datetime  
+from sqlalchemy import create_engine  
+from sqlalchemy.orm import sessionmaker  
+from database import BitcoinPrice, Base  
+import os  
+from dotenv import load_dotenv  
 from flask import Flask, jsonify  
-import threading 
+import threading   
 
+# Inicializa o aplicativo Flask  
 app = Flask(__name__)  
 
-# Endpoint para a rota raiz  
-@app.route('/')  
-def index():  
-    return jsonify({"message": "Hello, World! Servidor funcionando!"}), 200  
+# Endpoint para verificação de saúde  
+@app.route('/health', methods=['GET'])  
+def health_check():  
+    return jsonify({"status": "running"}), 200  
 
-# Carrega as variáveis de ambiente
-load_dotenv()
+# Novo endpoint para a rota raiz  
+@app.route('/', methods=['GET'])  
+def root():  
+    return jsonify({"message": "Servidor funcionando! Utilize /health para verificar o status."}), 200  
 
-POSTGRES_USER = os.getenv('POSTGRES_USER')
-POSTGRES_PASSWORD = os.getenv('POSTGRES_PASSWORD')
-POSTGRES_HOST = os.getenv('POSTGRES_HOST')
-POSTGRES_PORT = os.getenv('POSTGRES_PORT')
-POSTGRES_DB = os.getenv('POSTGRES_DB')
+# Função para executar o aplicativo Flask  
+def run_app():  
+    app.run(host='0.0.0.0', port=5000)  
 
-DATABASE_URL = (f"postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}"
-               f"@{POSTGRES_HOST}:{POSTGRES_PORT}/{POSTGRES_DB}"
-)
+# Carrega as variáveis de ambiente  
+load_dotenv()  
 
-# Cria o engine e a sessão
-engine = create_engine(DATABASE_URL)
-Session = sessionmaker(bind=engine)
-# Função para criar a tabela bitcoin_prices
-def criar_tabela():
-    """Cria a tabela bitcoin_prices se ela não existir."""
-    Base.metadata.create_all(engine)
-    print("Tabela criada com sucesso!")
+POSTGRES_USER = os.getenv('POSTGRES_USER')  
+POSTGRES_PASSWORD = os.getenv('POSTGRES_PASSWORD')  
+POSTGRES_HOST = os.getenv('POSTGRES_HOST')  
+POSTGRES_PORT = os.getenv('POSTGRES_PORT')  
+POSTGRES_DB = os.getenv('POSTGRES_DB')  
 
-# Função para registro dos preços no banco de dados
-def salvar_dados_no_banco(dados):
-    """Salva os dados no banco de dados PostgreSQL."""
-    session = Session()
-    novo_registro = BitcoinPrice(**dados)
-    session.add(novo_registro)
-    session.commit()
-    session.close()
-    print(f"[{dados['timestamp']}] Dados salvos no PostgreSQL!")
+DATABASE_URL = (f"postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}"  
+               f"@{POSTGRES_HOST}:{POSTGRES_PORT}/{POSTGRES_DB}"  
+)  
 
-# Função para obter o preço atual do Bitcoin
-def get_bitcoin_price_binance():
-    # Endpoint da API da Binance para obter o preço atual do BTC/USDT
+# Cria o engine e a sessão  
+engine = create_engine(DATABASE_URL)  
+Session = sessionmaker(bind=engine)  
 
-    url = "https://api.binance.com/api/v3/ticker/price"
+# Função para criar a tabela bitcoin_prices  
+def criar_tabela():  
+    """Cria a tabela bitcoin_prices se ela não existir."""  
+    Base.metadata.create_all(engine)  
+    print("Tabela criada com sucesso!")  
 
-    params = {"symbol": "BTCUSDT"} 
+# Função para registro dos preços no banco de dados  
+def salvar_dados_no_banco(dados):  
+    """Salva os dados no banco de dados PostgreSQL."""  
+    session = Session()  
+    novo_registro = BitcoinPrice(**dados)  
+    session.add(novo_registro)  
+    session.commit()  
+    session.close()  
+    print(f"[{dados['timestamp']}] Dados salvos no PostgreSQL!")  
 
-    # Envia a requisição GET para a API
-    response = requests.get(url, params=params)
+# Função para obter o preço atual do Bitcoin  
+def get_bitcoin_price_binance():  
+    """Obtém o preço atual do Bitcoin da API da Binance."""  
+    url = "https://api.binance.com/api/v3/ticker/price"  
+    params = {"symbol": "BTCUSDT"}   
 
-    # Registra o momento de aquisição do valor
-    timestamp = datetime.now()
+    # Envia a requisição GET para a API  
+    response = requests.get(url, params=params)  
 
-    # Verifica se a requisição foi bem-sucedida
-    if response.status_code == 200:
-        data = response.json()
-        price = round(float(data["price"]), 2)
-        return {'timestamp': timestamp, 
-                'symbol': "BTC/USDT",
-                'price': price}
-    else:
-        print(f"Erro ao obter o preço do Bitcoin: {response.status_code}")
-        return None
+    # Registra o momento de aquisição do valor  
+    timestamp = datetime.now()  
 
-def worker():  
-    """Função que coleta dados de Bitcoin a cada 15 segundos."""  
+    # Verifica se a requisição foi bem-sucedida  
+    if response.status_code == 200:  
+        data = response.json()  
+        price = round(float(data["price"]), 2)  
+        return {'timestamp': timestamp,   
+                'symbol': "BTC/USDT",  
+                'price': price}  
+    else:  
+        print(f"Erro ao obter o preço do Bitcoin: {response.status_code}")  
+        return None  
+
+if __name__  == "__main__":  
+    criar_tabela()  
+    print("Iniciando a coleta de dados...")  
+
+    # Inicia o servidor Flask em uma thread separada  
+    threading.Thread(target=run_app).start()  
+
     while True:  
-        try:  
+        try:   
             dados = get_bitcoin_price_binance()  
             if dados:  
                 salvar_dados_no_banco(dados)  
@@ -85,15 +99,4 @@ def worker():
             time.sleep(15)  
         except Exception as e:  
             print(f"Erro ao coletar dados: {e}")  
-            time.sleep(15)  
-
-if __name__ == "__main__":  
-    criar_tabela()  
-    print("Iniciando a coleta de dados...")  
-
-    # Inicia a coleta de dados em uma thread separada  
-    data_thread = threading.Thread(target=worker)  
-    data_thread.start()  
-
-    # Executa o servidor Flask  
-    app.run(host='0.0.0.0', port=5000)  
+            time.sleep(15)
